3529aa4f172c0a8eaca85a984f25c3c902856935
Elisa De Benedettis <105230322+Giolti2@users.noreply.github.com>
2022-10-07 14:26:59 +0200

    Make it bounce
    -Added colors
    -Added text bounciness
    -Added limited amounts of silliness, if you will

--- sketch.js
+++ sketch.js
@@ -1,16 +1,78 @@
+const message = "This is a template repository\nfor the elective course Creative Coding\nCommunication Design, Politecnico di Milano";
+
+class BouncingText {
+	constructor() {
+		this.x = width / 2;
+		this.y = height / 2;
+		this.angle = random(30, 60);
+		this.speed = 5;
+		this.color = color(0,0,0)
+	}
+
+	update() {
+		this.x += cos(this.angle) * this.speed;
+		this.y += sin(this.angle) * this.speed;
+
+		//these checks need more texting to see if grouping conditions breaks edge cases or not
+		if (this.x < 0) {
+			if (this.angle > 90 && this.angle < 270) {
+				this.angle = 180 - this.angle;
+				this.color = color(random(100,255), random(100,255), random(100,255))
+				if (this.angle < 0) {
+					this.angle += 360;
+				}
+			}
+		}
+		else if (this.x > width) {
+			if (this.angle < 90 || this.angle > 270) {
+				this.angle = 180 - this.angle;
+				this.color = color(random(100,255), random(100,255), random(100,255))
+				if (this.angle < 0) {
+					this.angle += 360;
+				}
+			}
+		}
+
+		if (this.y < 0) {
+			if (this.angle > 180) {
+				this.angle = 360 - this.angle;
+				this.color = color(random(100,255), random(100,255), random(100,255))
+				if (this.angle < 0) {
+					this.angle += 360;
+				}
+			}
+		}
+		else if (this.y > height) {
+			if (this.angle < 180) {
+				this.angle = 360 - this.angle;
+				this.color = color(random(100,255), random(100,255), random(100,255))
+			}
+		}
+
+		push();
+		fill(this.color);
+		text(message, this.x, this.y);
+		pop();
+	}
+}
+
 function preload() {
 	// put preload code here
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
+	
+	angleMode(DEGREES)
+
 	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+	textSize(16);
+
+	window.startingText = new BouncingText();
 }
 
 function draw() {
 	// put drawing code here
+	background(255);
+	startingText.update();
 }
