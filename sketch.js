748a668dbd2d66b821cfd278188a190eb9e71f11
Elisa De Benedettis <105230322+Giolti2@users.noreply.github.com>
2022-10-10 16:14:57 +0200

    Now a real screensaver
    -Added input control
    -Added windows desktop mode
    -Added functions to draw responsive pixels, standalone and from an input set
    -Changed screensaver colors to be random HSB hues at max saturation
    -Added timer for system sleep

--- sketch.js
+++ sketch.js
@@ -1,23 +1,36 @@
 const message = "This is a template repository\nfor the elective course Creative Coding\nCommunication Design, Politecnico di Milano";
 
+const SLEEPTIME = 3000; //time to sleep in milliseconds
+
+//matrix to draw the windows logo from
+const WINDOWSLOGO = [[-1, -1, -1, -1, -1, -1, -1, -1, 		   -1, 			 0, 0, 0, 			0, 			-1, -1, -1],
+					 [-1, -1, -1, -1, -1, -1, -1,  0, 			0, 			 0, 0, 0, 			0, 			 0,  0, -1],
+					 [ 0, -1, -1, -1, -1, -1,  0,  0,           0, [255, 0, 0], 0, 0, [0, 255, 0], 			 0,  0,  0],
+					 [ 0, -1,  0, -1,  0,  0,  0,  0, [255, 0, 0], [255, 0, 0], 0, 0, [0, 255, 0], [0, 255, 0],  0,  0]];
+
+const START = [[]];
+
 class BouncingText {
 	constructor() {
 		this.x = width / 2;
 		this.y = height / 2;
 		this.angle = random(30, 60);
 		this.speed = 5;
-		this.color = color(0,0,0)
+		this.color = color(255)
 	}
 
 	update() {
 		this.x += cos(this.angle) * this.speed;
 		this.y += sin(this.angle) * this.speed;
 
+		push();
+		colorMode(HSB, 360, 100, 100)
+
 		//these checks need more texting to see if grouping conditions breaks edge cases or not
 		if (this.x < 64) {
 			if (this.angle > 90 && this.angle < 270) {
 				this.angle = 180 - this.angle;
-				this.color = color(random(100,200), random(100,200), random(100,200))
+				this.color = color(random(360), 100, 100)
 				if (this.angle < 0) {
 					this.angle += 360;
 				}
@@ -26,7 +39,7 @@ class BouncingText {
 		else if (this.x > width - 64) {
 			if (this.angle < 90 || this.angle > 270) {
 				this.angle = 180 - this.angle;
-				this.color = color(random(100,200), random(100,200), random(100,200))
+				this.color = color(random(360), 100, 100)
 				if (this.angle < 0) {
 					this.angle += 360;
 				}
@@ -36,7 +49,7 @@ class BouncingText {
 		if (this.y < 32) {
 			if (this.angle > 180) {
 				this.angle = 360 - this.angle;
-				this.color = color(random(100,200), random(100,200), random(100,200))
+				this.color = color(random(360), 100, 100)
 				if (this.angle < 0) {
 					this.angle += 360;
 				}
@@ -45,16 +58,18 @@ class BouncingText {
 		else if (this.y > height-32) {
 			if (this.angle < 180) {
 				this.angle = 360 - this.angle;
-				this.color = color(random(100,200), random(100,200), random(100,200))
+				this.color = color(random(360), 100, 100)
 			}
 		}
 
-		push();
 		fill(this.color);
 		text(message, this.x, this.y);
 		pop();
 	}
 }
+
+let wakeup = 0;
+let wakeupTime = 0;
 
 function preload() {
 	// put preload code here
@@ -62,6 +77,8 @@ function preload() {
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+
+	window.pixelSide = height / 480;
 	
 	angleMode(DEGREES)
 
@@ -72,7 +89,100 @@ function setup() {
 }
 
 function draw() {
-	// put drawing code here
-	background(255);
-	startingText.update();
+
+	checkTime();
+
+	if (wakeup) {
+		//desktop goes here
+		windows98();
+	}
+	else {
+		//screensaver goes here
+		background(0);
+		startingText.update();
+	}
+	
+}
+
+function mouseMoved() {
+	wakeUp();
+}
+
+function mousePressed() {
+	wakeUp();
+}
+
+function wakeUp() {
+	wakeup = 1;
+	wakeupTime = millis();
+}
+
+function checkTime() {
+	if ((millis() - wakeupTime) > SLEEPTIME) {
+		wakeup = 0;
+	}
+}
+
+//function to recreate the iconic w98 gui
+function windows98() {
+	background(43, 118, 98)
+
+	push();
+
+	noStroke();
+
+	//w98 taskbar
+	fill(193, 192, 193); 
+	rect(0, 452 * pixelSide, width, 28 * pixelSide);
+
+	fill(223);
+	rect(0, 452 * pixelSide, width, pixelSide);
+
+	fill(255);
+	rect(0, 453 * pixelSide, width, pixelSide);
+
+	//w98 start button
+	rect(2 * pixelSide, 456 * pixelSide, 53 * pixelSide, pixelSide)
+	rect(2 * pixelSide, 456 * pixelSide, pixelSide, 21 * pixelSide)
+
+	fill(223);
+	rect(3 * pixelSide, 457 * pixelSide, 51 * pixelSide, pixelSide)
+	rect(3 * pixelSide, 457 * pixelSide, pixelSide, 19 * pixelSide)
+
+	fill(128);
+	rect(3 * pixelSide, 476 * pixelSide, 52 * pixelSide, pixelSide)
+	rect(54 * pixelSide, 457 * pixelSide, pixelSide, 20 * pixelSide)
+
+	fill(0);
+	rect(2 * pixelSide, 477 * pixelSide, 54 * pixelSide, pixelSide)
+	rect(55 * pixelSide, 456 * pixelSide, pixelSide, 22 * pixelSide)
+
+	drawMatrix(6 * pixelSide, 460 * pixelSide, WINDOWSLOGO);
+	
+	pop();
+}
+
+//draws a square pixel scaled to canvas height, assumes canvas is 480 pixels high
+function drawPixel(x, y, color) {
+	push();
+
+	noStroke();
+	fill(color);
+	rect(pixelSide * x, pixelSide * y, pixelSide, pixelSide);
+
+	pop();
+}
+
+//draws pixels from a matrix of colors cause I don't want to write drawPixel a hundred times
+function drawMatrix(x, y, matrix) {
+	push();
+	translate(x, y);
+	for (let i = 0; i < matrix.length; i++){
+		for (let j = 0; j < matrix[i].length; j++){
+			if (matrix[i][j] != -1) {
+				drawPixel(j, i, matrix[i][j]);
+			}
+		}
+	}
+	pop();
 }
